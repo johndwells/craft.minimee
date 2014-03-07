a0b2f5dbb486152b5efb5f04e9c26b62efcb6505
John D Wells <johndwells@gmail.com>
2014-03-07 11:19:59 +0000

    Consolidate Minimee_CacheModel into MinimeeService.

--- services/MinimeeService.php
+++ services/MinimeeService.php
@@ -15,11 +15,12 @@ namespace Craft;
  */
 class MinimeeService extends BaseApplicationComponent
 {
-    public $assets;
-    public $type;
-
-    // read-only config
-    private $_config;
+    protected $_assets = array();
+    protected $_type = '';
+    protected $_cacheFilename           = '';       // lastmodified value for cache
+    protected $_cacheFilenameHash       = '';       // a hash of all asset filenames together
+    protected $_cacheTimestamp          = '';       // eventual filename of cache
+    protected $_config;
 
     // --------------------------
 
@@ -38,6 +39,16 @@ class MinimeeService extends BaseApplicationComponent
 
         // this will only be done once
         $this->_config = Minimee_ConfigModel::populateModel($plugin->getSettings());
+    }
+
+    public function getAssets()
+    {
+        return $this->_assets;
+    }
+
+    public function getType()
+    {
+        return $this->_type;
     }
 
     /**
@@ -94,8 +105,8 @@ class MinimeeService extends BaseApplicationComponent
 
     public function reset()
     {
-        $this->assets = null;
-        $this->type = null;
+        $this->assets = array();
+        $this->type = '';
 
         return $this;
     }
@@ -118,7 +129,7 @@ class MinimeeService extends BaseApplicationComponent
                     'type' => $this->type
                 );
 
-                $this->assets[] = Minimee_RemoteAssetModel::populateModel($model);
+                $this->_assets[] = Minimee_RemoteAssetModel::populateModel($model);
             }
             else
             {
@@ -127,7 +138,7 @@ class MinimeeService extends BaseApplicationComponent
                     'type' => $this->type
                 );
 
-                $this->assets[] = Minimee_LocalAssetModel::populateModel($model);
+                $this->_assets[] = Minimee_LocalAssetModel::populateModel($model);
             }
         }
 
@@ -157,7 +168,6 @@ class MinimeeService extends BaseApplicationComponent
 
     public function checkHeaders()
     {
-        // TODO: ensure each asset exists
         foreach($this->assets as $asset)
         {
             if( ! $asset->exists())
@@ -176,20 +186,113 @@ class MinimeeService extends BaseApplicationComponent
 
     public function cache()
     {
-        $cache = Minimee_CacheModel::populateModel(array(
-            'assets' => $this->assets,
-            'type' => $this->type,
-            'cachePath' => $this->config->cachePath,
-            'cacheUrl' => $this->config->cacheUrl)
-        );
-
-        if( ! $cache->exists())
+        if( ! $this->cacheExists())
         {
-            return $cache->create();
+            return $this->createCache();
         }
 
-        return $this->config->cacheUrl . $cache->filename;
+        return $this->cacheFilenameUrl;
     }
 
 
+    public function getCacheTimestamp()
+    {
+        return ($this->_cacheTimestamp == 0) ? '0000000000' : $this->_cacheTimestamp;
+    }
+
+    public function getCacheFilename()
+    {
+        if( ! $this->_cacheFilename)
+        {
+            $this->_cacheFilename = $this->cacheFilenameHash . '.' . $this->cacheTimestamp . '.' . $this->type;
+        }
+
+        return $this->_cacheFilename;
+    }
+
+    public function getCacheFilenameHash()
+    {
+        return sha1($this->_cacheFilenameHash);
+    }
+
+    public function setCacheTimestamp(DateTime $lastTimeModified)
+    {
+        $timestamp = $lastTimeModified->getTimestamp();
+        $this->_cacheTimestamp = max($this->cacheTimestamp, $timestamp);
+    }
+
+    public function setCacheFilenameHash($name)
+    {
+        // remove any cache-busting strings so the cache name doesn't change with every edit.
+        // format: .v.1330213450
+        $this->_cacheFilenameHash .= preg_replace('/\.v\.(\d+)/i', '', $name);
+    }
+
+    public function createCache()
+    {
+        // the eventual contents of our cache
+        $contents = '';
+        
+        foreach($this->assets as $asset)
+        {
+            $contents .= craft()->minimee->minify($asset);
+        }
+
+        IOHelper::writeToFile($this->config->cachePath . $this->cacheFilename, $contents);
+
+        $this->cleanupCache();
+
+        return $this->config->cacheUrl . $this->cacheFilename;
+    }
+
+    public function cleanupCache()
+    {
+        // only run cleanup if in devmode...?
+        if( ! craft()->config->get('devMode')) return;
+
+        $files = IOHelper::getFiles($this->config->cachePath);
+
+        foreach($files as $file)
+        {
+            $filenamePath = $this->config->cachePath . $this->cacheFilename;
+
+            // skip self
+            if ($file === $filenamePath) continue;
+
+            $filenameHashPath = $this->config->cachePath . $this->cacheFilenameHash;
+
+            if (strpos($file, $filenameHashPath) === 0)
+            {
+                // suppress errors by passing true as second parameter
+                IOHelper::deleteFile($file, true);
+            }
+        }
+    }
+
+    public function getCacheFilenamePath()
+    {
+        return $this->config->cachePath . $this->cacheFilename;
+    }
+
+    public function getCacheFilenameUrl()
+    {
+        return $this->config->cacheUrl . $this->cacheFilename;
+    }
+
+    public function cacheExists()
+    {
+        // loop through our files once
+        foreach ($this->assets as $asset)
+        {
+            $this->cacheTimestamp = $asset->lastTimeModified;
+            $this->cacheFilenameHash = $asset->filename;
+        }
+
+        if( ! IOHelper::fileExists($this->cacheFilenamePath))
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
