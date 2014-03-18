6d5f9f1e67a103dd1bf67122b3f38065814ecf81
John D Wells <johndwells@gmail.com>
2014-03-18 08:04:29 +0000

    Unit tests for Minimee_SettingsModel.

---
+++ plugins/minimee/tests/MinimeeSettingsModelTest.php
@@ -0,0 +1,120 @@
+<?php
+namespace Craft;
+
+use \Mockery as m;
+
+class MinimeeSettingsModelTest extends BaseTest
+{
+	protected $config;
+	protected $craft;
+
+	/**
+	 * Called at the start of each test run; helps bootstrap our tests
+	 *
+	 * @return void
+	 */
+	public function setUp()
+	{
+		require_once dirname(__FILE__) . '/../models/Minimee_SettingsModel.php';
+	}
+
+	public function testUseResourceCacheWhenBothNonEmpty()
+	{
+		$this->_populateWith(array(
+			'cachePath' => '/path/to/cache',
+			'cacheUrl' => 'http://domain.com/cache'
+		));
+
+		$this->assertEquals(false, $this->model->useResourceCache());
+	}
+
+	public function testUseResourceCacheWhenOneIsEmpty()
+	{
+		$this->_populateWith(array(
+			'cachePath' => '',
+			'cacheUrl' => 'http://domain.com/cache'
+		));
+
+		$this->assertEquals(false, $this->model->useResourceCache());
+
+		$this->_populateWith(array(
+			'cachePath' => '/path/to/cache',
+			'cacheUrl' => ''
+		));
+
+		$this->assertEquals(false, $this->model->useResourceCache());
+	}
+
+	public function testUseResourceCacheWhenBothEmpty()
+	{
+		$this->_populateWith(array(
+			'cachePath' => '',
+			'cacheUrl' => ''
+		));
+
+		$this->assertEquals(true, $this->model->useResourceCache());
+	}
+
+	public function testValidateCachePathAndUrlWhenBothEmpty()
+	{
+		$this->_populateWith(array(
+			'cachePath' => '',
+			'cacheUrl' => ''
+		));
+
+		$this->model->validateCachePathAndUrl();
+		$this->assertEquals(false, $this->model->hasErrors());
+	}
+
+	public function testValidateCachePathAndUrlWhenBothNonEmpty()
+	{
+		$this->_populateWith(array(
+			'cachePath' => '/path/to/cache',
+			'cacheUrl' => 'http://domain.com/cache'
+		));
+
+		$this->model->validateCachePathAndUrl();
+		$this->assertEquals(false, $this->model->hasErrors());
+	}
+
+/*
+Currently not working because when we encounter a validation error,
+Minimee_SettingsModel uses Craft::t('') when creating the errors array,
+and I haven't figured out how to mock Craft or t().
+
+	public function testValidateCachePathAndUrlWhenOneIsEmpty()
+	{
+		$this->_populateWith(array(
+			'cachePath' => '',
+			'cacheUrl' => 'http://domain.com/cache'
+		));
+
+		$this->model->validateCachePathAndUrl();
+		$this->assertEquals(true, $this->model->hasErrors());
+
+		$this->_populateWith(array(
+			'cachePath' => '/path/to/cache',
+			'cacheUrl' => ''
+		));
+
+		$this->model->validateCachePathAndUrl();
+		$this->assertEquals(true, $this->model->hasErrors());
+	}
+*/
+
+	protected function _inspect($data)
+	{
+		fwrite(STDERR, print_r($data));
+	}
+
+	/**
+	 * Internal method for shorthand populating our Minimee_SettingsModel
+	 * 
+	 * @param Array $attributes
+	 * @return Minimee_SettingsModel
+	 */
+	protected function _populateWith($attributes)
+	{
+		$this->model = Minimee_SettingsModel::populateModel($attributes);
+	}
+}
